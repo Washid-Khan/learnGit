beb9cf12975896629582d9356932dc3f00c1aad1
Washid Khan <washidkhan9@gmail.com>
2021-09-21 15:05:50 +0530

    hospital file hospital.java

---
+++ src/learnGit/hospital.java
@@ -0,0 +1,11 @@
+package learnGit;
+
+public class hospital {
+
+	public static void main(String[] args) {
+		String nameHospital = "OrthoMaxHospital";
+		System.out.println(nameHospital);
+
+	}
+
+}
