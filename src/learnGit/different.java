6aba76c5e9223b7dbff8b6e602b2f74018f38b45
Washid Khan <washidkhan9@gmail.com>
2021-09-21 10:28:12 +0530

    new file added different.java

---
+++ src/learnGit/different.java
@@ -0,0 +1,15 @@
+package learnGit;
+
+public class different {
+
+	public static void main(String[] args) {
+		
+		int a = 50;
+		int b = 100;
+		
+		int c = a+b;
+		System.out.println("sum of two number is ::"+c);
+
+	}
+
+}
