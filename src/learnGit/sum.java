a271ef0cc52db32a70a6aaed4bb5ae47a13acc24
Washid Khan <washidkhan9@gmail.com>
2021-09-21 10:33:32 +0530

    added message in sum.java

--- src/learnGit/sum.java
+++ src/learnGit/sum.java
@@ -7,10 +7,10 @@ public class sum {
 		int a = 50;
 		int b = 100;
 		int c = 90;
-		 int sum = a+b+c;
+		int sum = a+b+c;
 		 
 		 
-		 System.out.println("sum of two number is ::" + sum);
+		 System.out.println("sum of two number is ::" + sum + 9);
 
 	}
 
