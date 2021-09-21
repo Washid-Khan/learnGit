81f6a8f42fccd832dcc70b1709b9d0117203a945
Washid Khan <washidkhan9@gmail.com>
2021-09-21 11:27:55 +0530

    added multiply.java

---
+++ src/learnGit/multiply.java
@@ -0,0 +1,14 @@
+package learnGit;
+
+public class multiply {
+
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b = 10;
+		
+		System.out.println("multiply of two number is ::" + (a*b) );
+
+	}
+
+}
